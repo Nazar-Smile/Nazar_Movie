4903059210e7e96484dbde18f12539190df9f0b3
Nazar <116882052+Nazar-Smile@users.noreply.github.com>
2023-06-29 20:29:44 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -85,25 +85,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kinosite_project',
-        'USER': 'kinosite_project_user',
-        'PASSWORD': 'pulsar',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kinosite_project',
+#         'USER': 'kinosite_project_user',
+#         'PASSWORD': 'pulsar',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 AUTHENTICATION_BACKENDS = (
